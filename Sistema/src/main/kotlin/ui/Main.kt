60a35a65e11431609bf6de5274b5b4708f985b7a
Mário Luiz  Guimarães <103617965+marioluizguimaraes@users.noreply.github.com>
2025-02-20 09:57:46 -0300

    ;

--- Sistema/src/main/kotlin/ui/Main.kt
+++ Sistema/src/main/kotlin/ui/Main.kt
@@ -7,22 +7,22 @@ import services.SistemaChamados
 fun main() {
     val sistema = SistemaChamados()
 
-    /*sistema.cadastrarCliente(
-        1,
-        "Mário Luiz",
-        "706.649.100-70",
-        "mario@email.com",
-        "(84) 9999-3334",
-        "Serrambi II, Rua Desportista Jeremias Farias Lacerda, 1630, 59091250, Apartamento 303 do Bloco 13, Natal, Rio Grande do Norte"
-    )
-    sistema.cadastrarCliente(
-        2,
-        "João Silva",
-        "345.674.723-42",
-        "joao@email.com",
-        "(84) 9999-3333",
-        "Serrambi II, Rua Desportista Jeremias Farias Lacerda, 1630, 59091250, Apartamento 202 do Bloco 21, Natal, Rio Grande do Norte"
-    )*/
+    /*
+      1
+      Mário Luiz
+      706.649.100-70
+      mario@email.com
+      (84) 9999-3334
+      Serrambi II, Rua Desportista Jeremias Farias Lacerda, 1630, 59091250, Apartamento 303 do Bloco 13, Natal, Rio Grande do Norte
+
+      2
+      João Silva
+      345.674.723-42
+      joao@email.com
+      (84) 9999-3333
+      Serrambi II, Rua Desportista Jeremias Farias Lacerda, 1630, 59091250, Apartamento 202 do Bloco 21, Natal, Rio Grande do Norte
+
+    */
 
     sistema.cadastrarCliente(Forms.cliente())
     sistema.cadastrarTecnico(Forms.tecnico())
